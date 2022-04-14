8f35579444598d6277a415da540069f7b16763b6
Joshua Walker <93017323+jwkr2004@users.noreply.github.com>
2022-04-13 17:02:50 -0700

    Add files via upload

---
+++ JS/practice.js
@@ -0,0 +1,23 @@
+$( function() {
+  $( "#skills" ).accordion({
+    collapsible: true
+  });
+} );
+
+$( function() {
+  $( "#knowledge1" ).progressbar({
+    value: 15
+  });
+} );
+
+$( function() {
+  $( "#knowledge2" ).progressbar({
+    value: 65
+  });
+} );
+
+$( function() {
+  $( "#knowledge3" ).progressbar({
+    value: 100
+  });
+} );
